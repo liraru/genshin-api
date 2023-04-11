33513ffe7f22f738a63c1aec3e520f2955cd853b
Lorena Sineiro <losingar@gmail.com>
2023-04-11 11:09:58 +0200

    characters table, user_abyss_teams table definitions

--- data/mysql.script.sql
+++ data/mysql.script.sql
@@ -11,7 +11,7 @@ CREATE TABLE
     PRIMARY KEY (`id`),
     FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL
   );
-
+:
 CREATE TABLE
   `user_characters` (
     `id` INT NOT NULL AUTO_INCREMENT,
@@ -28,6 +28,43 @@ CREATE TABLE
     PRIMARY KEY (`id`),
     FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL,
     FOREIGN KEY (`assigned_weapon`) REFERENCES `user_weapons` (`id`) ON DELETE SET NULL
+  );
+
+CREATE TABLE
+  `characters` (
+    `name` VARCHAR(512) UNIQUE NOT NULL,
+    `title` VARCHAR(512),
+    `description` VARCHAR(500),
+    `element` VARCHAR(10),
+    `birthday` VARCHAR(5),
+    `ratity` CHAR(1),
+    `gender` VARCHAR(10),
+    `region` VARCHAR(20),
+    `affiliation` VARCHAR(20),
+    `constellation` VARCHAR(30),
+    `weapontype` VARCHAR(20),
+    `substat` VARCHAR(30),
+    `icon` VARCHAR(512),
+    `gacha_splash` VARCHAR(512),
+    PRIMARY KEY (`name`)
+  );
+
+CREATE TABLE
+  `user_abyss_teams` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `user_id` INT,
+    `name` VARCHAR (512),
+    `character_1` VARCHAR(512),
+    `character_2` VARCHAR(512),
+    `character_3` VARCHAR(512),
+    `character_4` VARCHAR(512),
+    `WIP` TINYINT(1) DEFAULT 0,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL,
+    FOREIGN KEY (`character_1`) REFERENCES `characters` (`name`) ON DELETE NO ACTION,
+    FOREIGN KEY (`character_2`) REFERENCES `characters` (`name`) ON DELETE NO ACTION,
+    FOREIGN KEY (`character_3`) REFERENCES `characters` (`name`) ON DELETE NO ACTION,
+    FOREIGN KEY (`character_4`) REFERENCES `characters` (`name`) ON DELETE NO ACTION
   );
 
 CREATE TABLE
