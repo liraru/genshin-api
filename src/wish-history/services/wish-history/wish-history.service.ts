0f447ec66e0b4fc0dc8b45a1921bdaa7d47001eb
Lorena Sineiro <losingar@gmail.com>
2023-04-15 08:43:31 +0200

    moving wish history query builders to a separate service

--- src/wish-history/services/wish-history/wish-history.service.ts
+++ src/wish-history/services/wish-history/wish-history.service.ts
@@ -4,70 +4,31 @@ import { Observable, forkJoin, from, map } from 'rxjs';
 import { WishHistory } from 'src/entities/wish-history.entity';
 import { BANNERS, STANDARD_CHARACTERS } from 'src/wish-history/constants';
 import { IFiveStarHistory, IFiveStarPity, IPity } from 'src/wish-history/interfaces/pity.interface';
-import { DataSource, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
+import { WishHistoryQueryBuildersService } from '../wish-history-query-builders/wish-history-query-builders.service';
 
 @Injectable()
 export class WishHistoryService {
   constructor(
     @InjectRepository(WishHistory)
     private _wishHistoryRepo: Repository<WishHistory>,
-    private readonly _dataSource: DataSource
+    private readonly _qbService: WishHistoryQueryBuildersService
   ) {}
-
-  private async _retrievePityFromDB(banner: string, rarity: number): Promise<number> {
-    /*
-      SELECT COUNT(Name) AS Pity FROM user_wish_history uwh 
-      WHERE Banner = 'Character Event'
-        AND Time > (
-          SELECT Time 
-          FROM user_wish_history uwh 
-          WHERE banner = 'Character Event' AND Rarity = 5 
-          ORDER BY Time DESC, id DESC LIMIT 1)
-    */
-    const lastFiveStarQB = await this._dataSource
-      .getRepository(WishHistory)
-      .createQueryBuilder('pityQBSubquery')
-      .select('pityQBSubquery.Time')
-      .where(`pityQBSubquery.banner = '${banner}'`)
-      .andWhere(`pityQBSubquery.rarity = ${rarity}`)
-      .orderBy('pityQBSubquery.Time', 'DESC')
-      .limit(1)
-      .getOne();
-
-    const pityQB = await this._dataSource
-      .getRepository(WishHistory)
-      .createQueryBuilder('pityQB')
-      .select('COUNT(pityQB.Name)', 'count')
-      .where(`pityQB.banner = '${banner}'`)
-      .andWhere(`pityQB.Time > '${lastFiveStarQB?.Time ?? '2020'}'`)
-      .getRawOne();
-
-    return pityQB?.count ?? 0;
-  }
 
   private _checkFiftyWon(character: string, date: string, previousWon: boolean): boolean {
     const standard = STANDARD_CHARACTERS.find((f) => f.character === character);
-    
-    if ((!previousWon && standard) || (standard?.character === character && (standard?.promoReleaseEnd ?? '2020' > date))) {
+
+    if (
+      (!previousWon && standard) ||
+      (standard?.character === character && (standard?.promoReleaseEnd ?? '2020' > date))
+    ) {
       return false;
     }
     return true;
   }
 
   private async _retrieveFiveStarsHistory(banner: string) {
-    /*
-        SELECT Name, Pity, Time FROM user_wish_history uwh 
-        WHERE Rarity = 5 AND Banner = 'Character Event' 
-        ORDER BY `Time` DESC
-    */
-
-    const fiveStarPullQB = await this._dataSource
-      .getRepository(WishHistory)
-      .createQueryBuilder('fiveStarPullQB')
-      .where(`fiveStarPullQB.Rarity = 5 AND fiveStarPullQB.Banner = '${banner}'`)
-      .orderBy(`fiveStarPullQB.Time`, `ASC`)
-      .getMany();
-
+    const fiveStarPullQB = await this._qbService.getFiveStarsHistory(banner);
     const fiveStarPulls: IFiveStarPity[] = [];
     let previousRollWon: boolean;
 
@@ -90,11 +51,11 @@ export class WishHistoryService {
 
   getPity(): Observable<IPity> {
     return forkJoin({
-      char5: from(this._retrievePityFromDB(BANNERS.CHARACTERS, 5)),
-      char4: from(this._retrievePityFromDB(BANNERS.CHARACTERS, 4)),
-      weapon: from(this._retrievePityFromDB(BANNERS.WEAPONS, 5)),
-      standard5: from(this._retrievePityFromDB(BANNERS.STANDARD, 5)),
-      standard4: from(this._retrievePityFromDB(BANNERS.STANDARD, 4))
+      char5: from(this._qbService.getCurrentPity(BANNERS.CHARACTERS, 5)),
+      char4: from(this._qbService.getCurrentPity(BANNERS.CHARACTERS, 4)),
+      weapon: from(this._qbService.getCurrentPity(BANNERS.WEAPONS, 5)),
+      standard5: from(this._qbService.getCurrentPity(BANNERS.STANDARD, 5)),
+      standard4: from(this._qbService.getCurrentPity(BANNERS.STANDARD, 4))
     }).pipe(
       map((res) => {
         return {
