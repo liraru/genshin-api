30a74e5af3a518dab2fc70473203e01df7e2ff5a
Lorena Sineiro <losingar@gmail.com>
2023-05-07 18:50:14 +0200

    import file on demand, wip upload file

---
+++ src/modules/wish-history/interfaces/excel.interface.ts
@@ -0,0 +1,28 @@
+export interface ITypeAmount {
+  type: string;
+  amount: number;
+}
+
+export interface IExcelRow {
+  A: string;
+  B: string;
+  C: string;
+  D: number;
+  E: number;
+  F: number;
+  G: number;
+  H: string;
+  I: string;
+}
+
+export interface IExcelRowTitle {
+  Type: string;
+  Name: string;
+  Time: string;
+  Rarity: number;
+  Pity: number;
+  Roll: number;
+  Group: number;
+  Title: string;
+  Part: string;
+}
